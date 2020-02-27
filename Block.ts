a99bc021dea32898b38e33ec471401c38babf40b
Meero Harootunian <meero.harootunian@gmail.com>
2020-02-26 21:40:28 -0700

    add nonce to Block.ts

--- Block.ts
+++ Block.ts
@@ -2,17 +2,21 @@ import * as crypto from 'crypto'
 
 class Block {
     readonly hash: string;
+    readonly nonce: number;
 
     constructor(readonly index: number,
                 readonly previousHash: string,
                 readonly timestamp: number,
                 readonly data: string
                 ) {
-        this.hash = this.calculateHash();
+
+        const { nonce, hash } = this.mine();
+        this.nonce = nonce;
+        this.hash = hash;
     }
 
-    private calculateHash(): string {
-        const data = this.index + this.previousHash + this.timestamp + this.data;
+    private calculateHash(nonce: number): string {
+        const data = this.index + this.previousHash + this.timestamp + this.data + nonce;
         return crypto
             .createHash('sha256')
             .update(data)
